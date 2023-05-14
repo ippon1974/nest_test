fb25cb3acadd79b9907459b8c18543ed49dd415f
Vovan <plategi@yandex.ru>
2023-05-14 13:56:21 +0300

    o

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,32 +1,42 @@
 import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put } from '@nestjs/common';
 import {CreateProductDto} from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
 
+  constructor(private readonly productsService:ProductsService){
+
+  }
+
+  // @Get()
+  // getAll(){
+  //   return {
+  //     id: 1,
+  //     name: 'Senao 458 R Ultra',
+  //     country: 'Russia',
+  //     city: 'Moscow',
+  //     email: 'info@radiotel.info',
+  //     prise: 888,
+  //     delivery: true
+  //   };
+  // }
+
   @Get()
   getAll(){
-    return {
-      id: 1,
-      name: 'Senao 458 R Ultra',
-      country: 'Russia',
-      city: 'Moscow',
-      email: 'info@radiotel.info',
-      prise: 888,
-      delivery: true
-    };
+    return this.productsService.getAll()
   }
 
   @Get(':id')
-  getOne(@Param('id') id:string):string{
-      return 'getOne '  + id
+  getOne(@Param('id') id:string){
+      return this.productsService.getById(id)
   }
 
   @Post()
   @HttpCode(HttpStatus.CREATED)
-  create(@Body() createProductDto:CreateProductDto):string{
-    return `Title: ${createProductDto.title} Cost: ${createProductDto.price}`
+  create(@Body() createProductDto:CreateProductDto){
+    return this.productsService.create(createProductDto)
   }
 
   @Delete(':id')
---
+++ src/products/products.module.ts
@@ -0,0 +1,13 @@
+import { Module } from "@nestjs/common";
+import { ProductsService } from "./products.service";
+import { ProductsController } from "./products.controller";
+
+@Module({
+    providers: [ProductsService],
+    controllers: [ProductsController]
+
+})
+
+export class ProductsModule{
+
+}
